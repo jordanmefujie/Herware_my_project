5f852a790796aff2915d59158ec63cbe738e47fa
jordanmefujie <mjordanlonpo@gmail.com>
2024-02-24 13:21:38 -0800

    Backend files

---
+++ route.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from device import DeviceManager
+from user import UserManager
+from userManager import UserManager
+
+app = Flask(__name__)
+
+# Define routes for the Herware project
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user_manager = UserManager(data['username'])
+    return user_manager.create(data)
+
+
+@app.route('/devices', methods=['POST'])
+def create_device():
+    data = request.get_json()
+    device_manager = DeviceManager(
+        data['device_id'],
+        data['name'],
+        data['user_id'])
+    return device_manager.create()
+
+
+@app.route('/users/<string:user_id>', methods=['GET'])
+def get_user(user_id):
+    user_manager = UserManager(user_id)
+    return user_manager.read(user_id)
+
+
+@app.route('/devices/<string:device_id>', methods=['GET'])
+def get_device(device_id):
+    device_manager = DeviceManager(device_id)
+    return device_manager.read(device_id)
+
+
+@app.route('/users/<string:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    user_manager = UserManager(user_id)
+    return user_manager.update(user_id, data)
+
+
+@app.route('/devices/<string:device_id>', methods=['PUT'])
+def update_device(device_id):
+    data = request.get_json()
+    device_manager = DeviceManager(device_id)
+    return device_manager.update(device_id, data)
+
+
+@app.route('/users/<string:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user_manager = UserManager(user_id)
+    return user_manager.delete(user_id)
+
+
+@app.route('/devices/<string:device_id>', methods=['DELETE'])
+def delete_device(device_id):
+    device_manager = DeviceManager(device_id)
+    return device_manager.delete(device_id)
