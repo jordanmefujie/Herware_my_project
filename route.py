8de35c469cc7167a16b1fd68f43c64fd3f4717e5
jordanmefujie <mjordanlonpo@gmail.com>
2024-02-25 23:41:12 -0800

    Added documentation for route handlers: PUT and DELETE methods

--- route.py
+++ route.py
@@ -1,29 +1,40 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
+"""
+Flask app for managing users and devices.
+
+All modules and classes should be properly documented.
+"""
+
+from flask import Flask, render_template, request
 from device import DeviceManager
 from user import UserManager
-from userManager import UserManager
 
 app = Flask(__name__)
 
 # Define routes for the Herware project
 
-
 @app.route('/')
 def index():
+    """
+    Renders the index.html template.
+    """
     return render_template('index.html')
-
 
 @app.route('/users', methods=['POST'])
 def create_user():
+    """
+    Creates a new user.
+    """
     data = request.get_json()
     user_manager = UserManager(data['username'])
     return user_manager.create(data)
 
-
 @app.route('/devices', methods=['POST'])
 def create_device():
+    """
+    Creates a new device.
+    """
     data = request.get_json()
     device_manager = DeviceManager(
         data['device_id'],
@@ -31,40 +42,52 @@ def create_device():
         data['user_id'])
     return device_manager.create()
 
-
 @app.route('/users/<string:user_id>', methods=['GET'])
 def get_user(user_id):
+    """
+    Retrieves information about a specific user.
+    """
     user_manager = UserManager(user_id)
     return user_manager.read(user_id)
 
-
 @app.route('/devices/<string:device_id>', methods=['GET'])
 def get_device(device_id):
+    """
+    Retrieves information about a specific device.
+    """
     device_manager = DeviceManager(device_id)
     return device_manager.read(device_id)
 
-
 @app.route('/users/<string:user_id>', methods=['PUT'])
 def update_user(user_id):
+    """
+    Updates information about a specific user.
+    """
     data = request.get_json()
     user_manager = UserManager(user_id)
     return user_manager.update(user_id, data)
 
-
 @app.route('/devices/<string:device_id>', methods=['PUT'])
 def update_device(device_id):
+    """
+    Updates information about a specific device.
+    """
     data = request.get_json()
     device_manager = DeviceManager(device_id)
     return device_manager.update(device_id, data)
 
-
 @app.route('/users/<string:user_id>', methods=['DELETE'])
 def delete_user(user_id):
+    """
+    Deletes a specific user.
+    """
     user_manager = UserManager(user_id)
     return user_manager.delete(user_id)
 
-
 @app.route('/devices/<string:device_id>', methods=['DELETE'])
 def delete_device(device_id):
+    """
+    Deletes a specific device.
+    """
     device_manager = DeviceManager(device_id)
     return device_manager.delete(device_id)
